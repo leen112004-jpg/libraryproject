cbfa635798500cb28ff01293fb239eeabdc7f323
leen1 <leen112004@gmail.com>
2026-08-05 05:23:12 +0300

    complete Lab9 with all code and screenshots

--- apps/bookmodule/models.py
+++ apps/bookmodule/models.py
@@ -5,3 +5,25 @@ class Book(models.Model):
     author = models.CharField(max_length=50)
     price = models.FloatField(default=0.0)
     edition = models.SmallIntegerField(default=1)
+
+class Address(models.Model):
+    city = models.CharField(max_length=50)
+
+class Card(models.Model):
+    card_number = models.IntegerField()
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=50)
+
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    code = models.IntegerField()
+
+class Student(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    card = models.OneToOneField(Card, on_delete=models.PROTECT)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    courses = models.ManyToManyField(Course)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)  
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Book
+from django.db.models import Q, Count, Sum, Avg, Max, Min
+from .models import Student, Address
+from django.db.models import Count, Max
+from .models import Department, Course, Student
+
 
 def index(request):
     name = request.GET.get("name") or "world!"
@@ -67,3 +72,66 @@ def complex_query(request):
         return render(request, 'bookmodule/bookList.html', {'books': mybooks})
     else:
         return render(request, 'bookmodule/index.html')
+
+def task1(request):
+    books = Book.objects.filter(Q(price__lte=80))
+    return render(request, 'bookmodule/bookList.html', {'books': books})
+
+def task2(request):
+    books = Book.objects.filter(Q(edition__gt=3) & (Q(title__icontains='co') | Q(author__icontains='co')))
+    return render(request, 'bookmodule/bookList.html', {'books': books})
+
+def task3(request):
+    books = Book.objects.filter(~Q(edition__gt=3) & ~(Q(title__icontains='co') | Q(author__icontains='co')))
+    return render(request, 'bookmodule/bookList.html', {'books': books})
+
+def task4(request):
+    books = Book.objects.all().order_by('title')
+    return render(request, 'bookmodule/bookList.html', {'books': books})
+
+def task5(request):
+    stats = {
+        'count': Book.objects.count(),
+        'total': Book.objects.aggregate(Sum('price'))['price__sum'],
+        'avg': Book.objects.aggregate(Avg('price'))['price__avg'],
+        'max': Book.objects.aggregate(Max('price'))['price__max'],
+        'min': Book.objects.aggregate(Min('price'))['price__min'],
+    }
+    return render(request, 'bookmodule/stats.html', {'stats': stats})
+
+def task6(request):
+    students = Student.objects.all()
+    return render(request, 'bookmodule/students.html', {'students': students})
+
+def task7(request):
+    city_stats = Address.objects.annotate(student_count=Count('student'))
+    return render(request, 'bookmodule/city_stats.html', {'city_stats': city_stats})
+
+def task6(request):
+    students = Student.objects.all()
+    return render(request, 'bookmodule/students.html', {'students': students})
+
+def task7(request):
+    city_stats = Address.objects.annotate(student_count=Count('student'))
+    return render(request, 'bookmodule/city_stats.html', {'city_stats': city_stats})
+
+
+def lab9_task1(request):
+    departments = Department.objects.annotate(student_count=Count('student'))
+    return render(request, 'bookmodule/lab9_task1.html', {'departments': departments})
+
+def lab9_task2(request):
+    courses = Course.objects.annotate(student_count=Count('student'))
+    return render(request, 'bookmodule/lab9_task2.html', {'courses': courses})
+
+def lab9_task3(request):
+    departments = Department.objects.annotate(oldest_student_id=Max('student__id'))
+    oldest_students = []
+    for dept in departments:
+        student = Student.objects.filter(department=dept).order_by('id').first()
+        oldest_students.append({'department': dept.name, 'student_name': student.name if student else 'None'})
+    return render(request, 'bookmodule/lab9_task3.html', {'oldest_students': oldest_students})
+
+def lab9_task4(request):
+    departments = Department.objects.annotate(student_count=Count('student')).filter(student_count__gt=2).order_by('-student_count')
+    return render(request, 'bookmodule/lab9_task4.html', {'departments': departments})
